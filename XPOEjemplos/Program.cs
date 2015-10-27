e1909dc149fdaed15c53d7793b27b6bf5dbf01ad
egarim <joche.ojeda@bitframeworks.com>
2015-10-27 08:19:08 -0600

    Creacion de productos

--- XPOEjemplos/Program.cs
+++ XPOEjemplos/Program.cs
@@ -19,6 +19,11 @@ namespace XPOEjemplos
             CrearCliente(DevExpress.Xpo.XpoDefault.Session, "Oscar Ojeda", "Santa Elena", "503 231458967", true);
             CrearCliente(DevExpress.Xpo.XpoDefault.Session, "Jaime Macias", "San Miguel", "503 258963147", true);
             CrearCliente(DevExpress.Xpo.XpoDefault.Session, "Juan Perez", "Santa Ana", "503 23654789", true);
+            //Creando productios
+            CrearProducto(DevExpress.Xpo.XpoDefault.Session, "Microsoft Surface", "Table hibrida", 900, 500);
+            CrearProducto(DevExpress.Xpo.XpoDefault.Session, "Macbook pro", "laptop de apple", 1200, 700);
+            CrearProducto(DevExpress.Xpo.XpoDefault.Session, "Arduino Uno", "Micro controlador italiando", 230, 800);
+            CrearProducto(DevExpress.Xpo.XpoDefault.Session, "Microsoft Surface", "Table hibrida", 600, 890);
         }
         private void CrearCliente(Session Session,string Nombre,string Direccion,string Telefono,bool Activo)
         {
@@ -38,13 +43,14 @@ namespace XPOEjemplos
             Cliente.Activo = Activo;
             
         }
-        private void CrearProductos(Session Session, string Descripcion, string Nombre, decimal PrecioUnitario, double CantidadEnInventario)
+        private void CrearProducto(Session Session,  string Nombre, string Descripcion, decimal PrecioUnitario, double CantidadEnInventario)
         {
             Producto Producto = new Producto(Session);
             Producto.Nombre = Nombre;
             Producto.Descripcion = Descripcion;
             Producto.PrecioUnitario = PrecioUnitario;
             Producto.CantidadEnInventario = CantidadEnInventario;
+            Producto.Save();
 
         }
     }
