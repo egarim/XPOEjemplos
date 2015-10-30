9d481e89416b2178c18b385f7761f5fa3ebcde44
egarim <joche.ojeda@bitframeworks.com>
2015-10-29 21:28:09 -0600

    Mas documentacion

--- XPOEjemplos/BusinessObjects/Cliente.cs
+++ XPOEjemplos/BusinessObjects/Cliente.cs
@@ -4,6 +4,29 @@ using DevExpress.Xpo;
 namespace XPOEjemplos.BusinessObjects
 {
 
+    /*
+    Tipos de datos soportados en xpo
+        https://documentation.devexpress.com/#CoreLibraries/CustomDocument2003
+    Como y por que XPO extiende el esquema creado en la base de datos
+       
+    -aqui se explica por que se crean las columnas, en todas las tablas:
+        OptimisticLockField 
+        GCRecord
+        Oid
+    -Control de concurrencia
+    https://documentation.devexpress.com/#CoreLibraries/CustomDocument2106
+    -Como procede XPO con las clases polimorficas (Caso de herencia)
+    https://documentation.devexpress.com/#CoreLibraries/CustomDocument2632
+
+    -Borrado logico o retardado, este esta implentado por default y aqui se explica como esta implementado
+        https://documentation.devexpress.com/#CoreLibraries/CustomDocument2103
+
+    -CodeRush templates para crear clases persistentes con shorcuts en el teclado
+        https://documentation.devexpress.com/#CodeRush/CustomDocument9279
+
+    -Como se refleja del herencia de clases en las tablas
+        https://documentation.devexpress.com/#CoreLibraries/CustomDocument2125
+    */
     public class Cliente : XPObject
     {
         public Cliente() : base()
---
+++ XPOEjemplos/BusinessObjects/MiClaseBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DevExpress.Xpo;
+
+namespace XPOEjemplos.BusinessObjects
+{
+    [NonPersistent()]
+    public class MiClaseBase : XPObject
+    {
+        public MiClaseBase() : base()
+        {
+            // This constructor is used when an object is loaded from a persistent storage.
+            // Do not place any code here.
+        }
+
+        public MiClaseBase(Session session) : base(session)
+        {
+            // This constructor is used when an object is loaded from a persistent storage.
+            // Do not place any code here.
+        }
+
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place here your initialization code.
+        }
+    }
+
+}
